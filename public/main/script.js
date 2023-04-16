7d370f7b64072c7892faa16fb6c12d0679126fe7
周旭 <zhouxusoft@gmail.com>
2023-04-16 17:05:05 +0800

    ..

--- public/main/script.js
+++ public/main/script.js
@@ -1,5 +1,41 @@
-let topmenu = document.getElementsByClassName("topmenu")[0]
+function handleClick(event) {
+    //点击菜单不触发事件
+    if (!event.target.classList.contains("filemenu") && !event.target.classList.contains("dirmenu")) {
+        alert(666)
+        // 选中事件点击过一次后，移除被选中属性并且移除监听事件
+        selected.classList.remove("selected")
+        selected.removeEventListener('click', handleClick)
+    }
+}
 
-topmenu.addEventListener('click', function () {
-    
-});
+var filedata = document.getElementsByClassName("filedata")
+var selected = null
+
+function selectedClick(event) {
+    console.log(event.target)
+    // console.log(this)
+    // 点击菜单不触发事件
+    if (!event.target.classList.contains("filemenu") && !event.target.classList.contains("dirmenu")) {
+        // 遍历所有 filedata 元素，判断当前点击的元素是否是 filedata 元素
+        for (var i = 0; i < filedata.length; i++) {
+            if (this === filedata[i]) {
+                // 把之前被选中的元素的 selected 类名和事件监听器移除
+                if (selected !== null && selected != this) {
+                    selected.classList.remove("selected");
+                    selected.removeEventListener("click", handleClick);
+                }
+                // 给当前被点击的元素添加 selected 类名，并存储
+                this.classList.add("selected");
+                selected = this;
+                // 给当前被点击的元素绑定事件监听器
+                this.addEventListener("click", handleClick);
+                break;
+            }
+        }
+    }
+}
+
+// 给所有 filedata 元素绑定点击事件监听器
+for (var i = 0; i < filedata.length; i++) {
+    filedata[i].addEventListener("click", selectedClick);
+}
