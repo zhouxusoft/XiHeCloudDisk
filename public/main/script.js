859a7871de6284cc609837223b60f78f1b8f4d05
周旭 <zhouxusoft@gmail.com>
2023-04-18 19:15:57 +0800

    ..

--- public/main/script.js
+++ public/main/script.js
@@ -34,14 +34,14 @@ function selectedClick(event) {
             if (this === filedata[i]) {
                 // 把之前被选中的元素的 selected 类名和事件监听器移除
                 if (selected !== null && selected != this) {
-                    selected.classList.remove("selected");
-                    selected.removeEventListener("click", handleClick);
+                    selected.classList.remove("selected")
+                    selected.removeEventListener("click", handleClick)
                 }
                 // 给当前被点击的元素添加 selected 类名，并存储
-                this.classList.add("selected");
+                this.classList.add("selected")
                 selected = this;
                 // 给当前被点击的元素绑定事件监听器
-                this.addEventListener("click", handleClick);
+                this.addEventListener("click", handleClick)
                 break;
             }
         }
@@ -52,16 +52,41 @@ function selectedClick(event) {
 
 // 给所有 filedata 元素绑定点击事件监听器
 for (var i = 0; i < filedata.length; i++) {
-    filedata[i].addEventListener("click", selectedClick);
+    filedata[i].addEventListener("click", selectedClick)
 }
 
 var filemenus = document.getElementsByClassName("filemenu")
 var filepops = document.getElementsByClassName("filepop")
 var dirmenus = document.getElementsByClassName("dirmenu")
 var dirpops = document.getElementsByClassName("dirpop")
+var rowborder = document.getElementsByClassName("rowborder")[0]
 
 for (let i = 0; i < dirmenus.length; i++) {
     dirmenus[i].addEventListener('mouseenter', function () {
-        console.log(666)
+        const rowborderP = rowborder.getBoundingClientRect()
+        const dirmenuP = dirmenus[i].getBoundingClientRect()
+
+        if (rowborderP.top + rowborderP.height / 2 < dirmenuP.top) {
+            dirpops[i].classList.remove("dirpopdown")
+            dirpops[i].classList.add("dirpopup")
+        } else {
+            dirpops[i].classList.remove("dirpopup")
+            dirpops[i].classList.add("dirpopdown")
+        }
+    })
+}
+
+for (let i = 0; i < filemenus.length; i++) {
+    filemenus[i].addEventListener('mouseenter', function () {
+        const rowborderP = rowborder.getBoundingClientRect()
+        const filemenuP = filemenus[i].getBoundingClientRect()
+
+        if (rowborderP.top + rowborderP.height / 2 < filemenuP.top) {
+            filepops[i].classList.remove("filepopdown")
+            filepops[i].classList.add("filepopup")
+        } else {
+            filepops[i].classList.remove("filepopup")
+            filepops[i].classList.add("filepopdown")
+        }
     })
 }
