cb676a16a543b767704538d4f74cb052b577e15e
周旭 <zhouxusoft@gmail.com>
2023-04-21 23:55:45 +0800

    ..

--- public/main/script.js
+++ public/main/script.js
@@ -121,6 +121,18 @@ function getdate(datetime) {
     return date
 }
 
+function formatFileSize(bytes) {
+    if (bytes < 1024) {
+        return bytes + " Bytes";
+    } else if (bytes < 1048576) {
+        return (bytes / 1024).toFixed(2) + " KB";
+    } else if (bytes < 1073741824) {
+        return (bytes / 1048576).toFixed(2) + " MB";
+    } else {
+        return (bytes / 1073741824).toFixed(2) + " GB";
+    }
+}
+
 function resetFilePage(filelist) {
     if (filelist.length > 0) {
         for (let i = 0; i < filelist.length; i++) {
